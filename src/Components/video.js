e8316cceb83d54cd50dd4894be9943cd0f471643
JP Baez <baezjuanpablo@gmail.com>
2021-08-11 14:49:46 +0200

    video and web pages setup

--- src/Components/video.js
+++ src/Components/video.js
@@ -1,7 +1,27 @@
+import './video.css'
+import ReactPlayer from 'react-player';
+
 
 const Video = () => {
     return( 
-        <p>Video</p>
+        <div className="video-body">
+            <p>The complete work is available in Beatkino.com</p>
+        
+            
+    
+                <ReactPlayer 
+                className="page-video"
+                url="https://vimeo.com/585754763"
+                controls={true}
+                responsive="true"
+                width="100%"
+                height="50vh"
+                /> 
+                
+            
+
+        </div>
+        
     )
 }
 
--- src/Components/web.js
+++ src/Components/web.js
@@ -1,7 +1,17 @@
+import './web.css';
+
+
 
 const Web = () => {
     return( 
-        <p>web</p>
+        <div className="web-body">
+            <p>jpbaez.com / TECHSTACK: React, CSS, Strapi (CMS)</p>
+            <p>beatkino.com / TECHSTACK: React, CSS, MongoDb</p>
+            <p>cl.com / TECHSTACK: React, CSS, Contentful (CMS)</p>
+
+
+        </div>
+        
     )
 }
 
