b8567d916285e215091d38516cafbb6986c48079
JP Baez <baezjuanpablo@gmail.com>
2021-08-18 20:22:12 +0200

    fix for deploy

--- src/Components/web.js
+++ src/Components/web.js
@@ -6,8 +6,8 @@ const Web = () => {
     return( 
         <div className="web-body">
             <p>jpbaez.com / TECHSTACK: React, CSS, Contentful(CMS)</p>
-            <p><a href="" >beatkino.com</a> (film stream platform) / TECHSTACK: React, CSS, MongoDb</p>
-            <p><a>cl.com</a> (personal portfolio) / TECHSTACK: React, CSS, Contentful (CMS)</p>
+            <p><a href="https://media.giphy.com/media/3o72FkiKGMGauydfyg/source.gif" >beatkino.com</a> (film stream platform) / TECHSTACK: React, CSS, MongoDb</p>
+            <p><a href="https://media.giphy.com/media/3o72FkiKGMGauydfyg/source.gif">cl.com</a> (personal portfolio) / TECHSTACK: React, CSS, Contentful (CMS)</p>
 
 
         </div>
