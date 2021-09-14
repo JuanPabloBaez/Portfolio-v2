64635b6b97d51dac031f0573f5cbfa8de10daac8
JP Baez <baezjuanpablo@gmail.com>
2021-09-14 20:13:50 +0200

    css fix

--- src/Components/web.js
+++ src/Components/web.js
@@ -8,7 +8,7 @@ const Web = () => {
             
             <span>jpbaez.com / <b>tech stack:</b> React, CSS, Contentful(CMS)</span>
             <span><a href="http://beatkino.com/" target="_blank"  rel="noreferrer">beatkino.com</a>  (film stream platform) / <b>tech stack:</b> React, CSS, MongoDb</span>
-            <span>{/* <a href="https://media.giphy.com/media/3o72FkiKGMGauydfyg/source.gif">cl.com</a> */} cl.cl (personal portfolio) / <b>tech stack:</b> React, CSS, Contentful (CMS)</span>
+            <span><a href="https://carloslertora.cl" target="_blank"  rel="noreferrer">carloslertora.cl</a>  (personal portfolio) / <b>tech stack:</b> React, CSS, Contentful (CMS)</span>
 
 
         </div>
