bae47504575be03fc047adf41e8ce8190076c263
JP Baez <baezjuanpablo@gmail.com>
2021-09-16 16:17:53 +0200

    new nav

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -7,11 +7,11 @@ const Nav = () => {
         <div className="nav-bar">    
             
             <div className="link-panel">
-                <Link to="/video" className="nav-button">video </Link>
-                <Link to="/web" className="nav-button">web </Link>
+                <Link to="/video" className="nav-button nav-video">video </Link>
+                <Link to="/web" className="nav-button nav-web">web </Link>
                 <Link to="/" className="logo"><img src={Logo} alt="logo" /></Link>
-                <Link to="/image" className="nav-button">image </Link>
-                <Link to="/board" className="nav-button">board </Link>
+                <Link to="/image" className="nav-button nav-image">image </Link>
+                <Link to="/board" className="nav-button nav-board">board </Link>
                 
             </div>
         </div>
