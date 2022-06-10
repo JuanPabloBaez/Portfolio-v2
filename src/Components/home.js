7be98a72ea522b9f76e542315fe0667ae0a67ef0
JP Baez <baezjuanpablo@gmail.com>
2022-06-10 11:56:25 +0200

    home update

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,5 +1,5 @@
 
-import videoHome from '../images/title_2.mp4';
+import videoHome from '../images/web_intro.mp4';
 import inLogo from '../images/iconos/linkedin.png';
 import mailLogo from '../images/iconos/email.png';
 import InstagramLogo from '../images/iconos/instagram.png';
@@ -14,9 +14,9 @@ const Home = () => {
             <div className="hello">   
                
                 <div className="greet">
+                    <div className='hi'>
                     <p>I'm Juan Pablo,</p>
                     <div className="greet-icons"> 
-                        
                         <svg id="i-code" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3">
                             <path d="M10 9 L3 17 10 25 M22 9 L29 17 22 25 M18 7 L14 27" />
                         </svg>
@@ -27,22 +27,23 @@ const Home = () => {
                         </svg>
                         video producer
                     </div> 
-                    <p>I also run  
-                        <a href="http://beatkino.com/" target="_blank"  rel="noreferrer"> beatkino.com</a> <br/> On this site you can find a diverse selection of my work. <br/> I'm currently based in Berlin.
-                    </p>  
-                </div>
-                <div className="social-icons">
-                        
-                        
+                    <p>I also run <a href="http://beatkino.com/" target="_blank"  rel="noreferrer"> beatkino.com</a> <br/> On this site you can find a diverse selection of my work. <br/> I'm currently based in Berlin.</p>  
+
+                    </div>
+                    
+                    <div className="social-icons">
                         <a href="mailto:contact@jpbaez.com" target="_blank"  rel="noreferrer" ><img className="social-icon" src={mailLogo} alt="vimeo logo"/></a>
                         <a href="https://www.linkedin.com/in/juan-pablo-baez-4b592235/" target="_blank"  rel="noreferrer" ><img className="social-icon" src={inLogo} alt="linkendin logo"/></a>
                         <a href="https://github.com/JuanPabloBaez" target="_blank"  rel="noreferrer" ><img className="social-icon" src={githubLogo} alt="github logo"/></a>
                         <a href="https://www.instagram.com/jp_baez/" target="_blank"  rel="noreferrer" ><img className="social-icon" src={InstagramLogo} alt="instagram logo"/></a>
                     </div>
-            </div>
-            <video id="background-video" autoPlay="autoplay" loop="loop" muted  playsInline >
+                </div>
+                
+                <video id="background-video" autoPlay="autoplay" loop="loop" muted  playsInline >
                     <source src={videoHome} type="video/mp4" />    
             </video>
+            </div>
+            
         </div>
         
     )
