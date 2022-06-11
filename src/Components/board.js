24d23f399f7d7b347bda7b35f2d836678de57f0f
JP Baez <baezjuanpablo@gmail.com>
2022-06-11 10:27:44 +0200

    new board css

--- src/Components/board.js
+++ src/Components/board.js
@@ -11,98 +11,88 @@ SwiperCore.use([Navigation]);
 const Board = ({posts}) => {
     
       
-    return( 
-        <div className="board-body">
-            <h1>Board</h1>
-            {
-                posts.map((post, index)=> {
+return( 
+    <div className="board-body">
+        <h1>Board</h1>
+        {posts.map((post, index)=> {
+            let{ header,
+                mainPicture,
+                date,
+                place,
+                placeLink,
+                mainContent1,
+                mainContent2,
+                videoLink,
+                media} = post.fields
+               
+            return(
+                <div className="post-container" key={index}>
+                    <h2>{header}</h2>
+                    <span>
+                        <p className="post-date">{date} </p>
+                        {(function() {
+                            if ( place && placeLink  ) {
+                                return <a className="post-place" href={placeLink} target="_blank" rel="noreferrer">on {place} </a>;  
+                            } if (place ==="") {
+                                return null
+                            } else {
+                                return <p className="post-place"> {place}</p>;
+                            }
+                        })()}
+                    </span>
+                    <div className="post-main-text">
+                        {mainPicture && <img className='main-photo' src={mainPicture.fields.file.url} alt={header}/>}
+                        <RichText 
+                            richText={mainContent1}
+                            overrides={{
+                                [BLOCKS.PARAGRAPH]: {
+                                    component: "p",
+                                    props: {
+                                    className: 'post-main-content'
+                                    }
+                                }
+                            }}
+                        />
+                    </div>
 
-                    return(
-                        <div className="post-container" key={index}>
-                            <div className="post-main">
-                                {post.fields.mainPicture && <img src={post.fields.mainPicture.fields.file.url} alt={post.fields.title}/>}
-                                <div className="post-main-text">
-                                    <RichText
-                                        richText={post.fields.title}
-                                        overrides={{
-                                            [BLOCKS.PARAGRAPH]: {
-                                              component: "h2",
-                                              props: {
-                                                className: 'board-post-title'
-                                              }
-                                            }
-                                          }}
-                                     />
-                                    <span>
-                                        <p className="post-date">{post.fields.date} </p>
-                                        {(function() {
-                                            if ( post.fields.place && post.fields.placeLink  ) {
-                                                return <a className="post-place" href={post.fields.placeLink} target="_blank" rel="noreferrer">on {post.fields.place} </a>;  
-                                            } if (post.fields.place ==="") {
-                                                return null
-                                            } else {
-                                                return <p className="post-place"> {post.fields.place}</p>;
-                                            }
-                                        })()}
-                                    </span>
-                                    <RichText 
-                                        richText={post.fields.mainContent1}
-                                        overrides={{
-                                            [BLOCKS.PARAGRAPH]: {
-                                              component: "p",
-                                              props: {
-                                                className: 'post-main-content'
-                                              }
-                                            }
-                                          }}
-                                    />
-                                </div>
-                            </div>
-                            
-
-                            {post.fields.media > 1 ? <Swiper
-                                spaceBetween={50}
-                                slidesPerView={1}
-                                height="100%"
-                                navigation
-                                loop={true}
-                            >
-                            {
-                                post.fields.media.map((photo, index )=> {
-                                return (
-                                <SwiperSlide key={index}>
-                                    <img className="board-photo" src={photo.fields.file.url}  alt="post related"  />
-                                </SwiperSlide>
-                                )
-                                })      
-                            }  
-                            </Swiper>:
-                            post.fields.media ? 
-                            <img className="board-photo" src={post.fields.media[0].fields.file.url}  alt="post related"  />:
-                            null
-} 
-                           
-                            {post.fields.mainContent2 ?  <RichText 
-                                                            richText={post.fields.mainContent2}
-                                                            overrides={{
-                                                                [BLOCKS.PARAGRAPH]: {
-                                                                component: "p",
-                                                                props: {
-                                                                    className: 'post-main-content2'
-                                                                }
-                                                                }
-                                                            }}
-                                                          /> : null}
-                            {post.fields.videoLink && <ReactPlayer url={post.fields.videoLink} className="board-player" width="100%" />}
-                            
-
-                            <hr></hr>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
+                    {media && media.length ===1 ? <img className="board-photo" src={post.fields.media[0].fields.file.url}  alt="post related"  />:null}
+                    {media && media.length > 1 ? <Swiper
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        height="100%"
+                        navigation
+                        loop={true}
+                    >
+                    { media.map((photo, index )=> {
+                        return (
+                            <SwiperSlide key={index}>
+                                <img className="board-photo" src={photo.fields.file.url}  alt="post related"  />
+                            </SwiperSlide>
+                        )
+                    })}  
+                    </Swiper>
+                    :null
+                    } 
+                    
+                    {mainContent2 ?  <RichText 
+                        richText={mainContent2}
+                        overrides={{
+                            [BLOCKS.PARAGRAPH]: {
+                            component: "p",
+                            props: {
+                                className: 'post-main-content2'
+                            }
+                            }
+                        }}
+                        /> : null
+                    }
+                    {videoLink && <ReactPlayer url={videoLink} className="board-player" width="100%" />}
+                    <hr></hr>
+                </div>
+            )
+        })}
+    </div>
+)
 }
 
 export default Board;
